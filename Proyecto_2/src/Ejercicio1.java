db311c72d05cec693899fbd11101fd78187ba311
Abudau <alexbudau97@gmail.com>
2023-12-11 21:26:29 +0100

    Cambio de ejemplo.

--- Proyecto_2/src/Ejercicio1.java
+++ Proyecto_2/src/Ejercicio1.java
@@ -2,7 +2,7 @@
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub.
 		System.out.println("Andrei");
 
 	}
